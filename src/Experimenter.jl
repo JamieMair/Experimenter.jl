e06ba66400298bb610c1e24032dfabc703fabdf0
Jamie Mair <JamieMair@users.noreply.github.com>
2022-11-04 15:17:30 +0000

    Restructed which parts of the API to export

--- src/Experimenter.jl
+++ src/Experimenter.jl
@@ -5,17 +5,32 @@ include("experiment.jl")
 include("database.jl")
 include("runner.jl")
 
-export open_db, export_db, Experiment, Trial
-export LinearVariable, LogLinearVariable, RepeatVariable, IterableVariable, MatchIterableVariable
-export get_experiment, get_experiments, get_trial, get_trials, get_experiment_by_name, complete_trial!, complete_trial_in_global_database, get_trials_by_name, get_trials_ids_by_name
-export execute_trial, execute_trial_and_save_to_db_async, get_results_from_trial_global_database
-export execute, Runner
-export SerialMode, MultithreadedMode, DistributedMode
+
+## API
+
+### Database
+export open_db, export_db
 export restore_from_db
 export merge_databases!
-export get_snapshots, latest_snapshot, save_snapshot!, mark_trial_as_incomplete!
-export get_ratio_completed_trials_by_name
-export @execute
 
+### Experiments
+export Experiment
+export get_experiment, get_experiments, get_experiment_by_name
+
+### Trials
+export get_trial, get_trials, get_trials_by_name, get_trials_ids_by_name, get_results_from_trial_global_database
+export complete_trial!, complete_trial_in_global_database, mark_trial_as_incomplete!
+
+### Execution
+export execute_trial, execute_trial_and_save_to_db_async
+export @execute
+export SerialMode, MultithreadedMode, DistributedMode
+
+### Snapshots
+export get_snapshots, latest_snapshot, save_snapshot!
+
+## Misc
+export LinearVariable, LogLinearVariable, RepeatVariable, IterableVariable, MatchIterableVariable
+export get_ratio_completed_trials_by_name
 
 end
